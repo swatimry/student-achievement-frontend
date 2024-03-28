92bdb8e71f75e2bc67e37d95fb1ab2934da01539
swati <swati2003mry@gmail.com>
2024-03-28 09:34:20 +0530

    server update

--- src/redux/store.js
+++ src/redux/store.js
@@ -13,4 +13,5 @@ const store = configureStore({
 
 
 export default store;
-export const server="http://localhost:4000";
+//export const server="http://localhost:4000";
+export const server="https://student-achievement-backend.onrender.com";
